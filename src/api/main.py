43d133224157228125a11c5594dd84afe410f81d
Balakumaran <balakumaran247@gmail.com>
2024-05-31 18:24:35 +0530

    added: result route

--- src/api/main.py
+++ src/api/main.py
@@ -1,8 +1,9 @@
-from typing import Dict
+from typing import Dict, List
 
 from fastapi import FastAPI
 
-from components.process import generate_question
+from components.helper import AnswerList
+from components.process import answer_check, generate_question
 
 __version__ = "1.0.0"
 
@@ -15,6 +16,14 @@ def root() -> Dict[str, str]:
 
 
 @app.get("/question")
-async def question() -> Dict[str, str]:
-    start, end = await generate_question()
+def question() -> Dict[str, str]:
+    start, end = generate_question()
     return {"start": start, "end": end}
+
+
+@app.post("/result")
+def result(answers: AnswerList) -> Dict[str, (str | List[str])]:
+    verdict, correct_list, wrong_list = answer_check(
+        answers.start, answers.end, answers.item
+    )
+    return {"result": verdict, "correct_path": correct_list, "wrong_path": wrong_list}
