0074623ec7fd6b345d771fe25f6b337c1de405b1
Balakumaran <balakumaran247@gmail.com>
2024-06-07 20:47:31 +0530

    added: App component

--- src/ui/src/App.js
+++ src/ui/src/App.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./App.css";
+import PageDynamics from "./Components/PageDynamics";
+import StatusContextProvider from "./Context/StatusContextProvider";
+import UrlContextProvider from "./Context/UrlContextProvider";
+import QuestionContextProvider from "./Context/QuestionContextProvider";
+import AnswerContextProvider from "./Context/AnswerContextProvider";
 
 function App() {
-  return (
-    <h1>Manu's Map Game</h1>
-  );
+    return (
+        <AnswerContextProvider>
+            <QuestionContextProvider>
+                <UrlContextProvider>
+                    <StatusContextProvider>
+                        <PageDynamics />
+                    </StatusContextProvider>
+                </UrlContextProvider>
+            </QuestionContextProvider>
+        </AnswerContextProvider>
+    );
 }
 
 export default App;
