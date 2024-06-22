da72d245f00d1ece8cd582a5c6953594c5df7c37
Balakumaran <balakumaran247@gmail.com>
2024-06-22 23:50:50 +0530

    fixed: marker image n position

--- src/ui/src/Components/ResultSection.js
+++ src/ui/src/Components/ResultSection.js
@@ -9,6 +9,18 @@ import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import Axios from "axios";
 
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
 const ResultSection = () => {
     let [status, setStatus] = useContext(StatusContext);
     const {
